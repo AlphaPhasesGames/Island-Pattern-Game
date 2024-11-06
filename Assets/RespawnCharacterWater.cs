4e2262615ce35acdb99b5fad0d8f20a9db19650b
WHawke <gothreepwood@gmail.com>
2024-11-06 15:05:58 +0000

    Added respawn script to stage 2 Added terrain and final stage enviroment.

---
+++ Assets/RespawnCharacterWater.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+namespace Pattern.Quest.Alpha.Phases.Games
+{
+    public class RespawnCharacterWater : MonoBehaviour
+    {
+        public GameObject respawnPoint;
+        public GameObject robotPlayer;
+        public CharacterController charCont;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            MovePlayer();
+        }
+
+        public void MovePlayer()
+        {
+            charCont.enabled = false;
+            robotPlayer.transform.rotation = respawnPoint.transform.rotation;
+            robotPlayer.transform.position = respawnPoint.transform.position;
+            charCont.enabled = true;
+        }
+    }
+}
