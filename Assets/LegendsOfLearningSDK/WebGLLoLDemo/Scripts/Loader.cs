dad588dc4628071aaa3b6d7274eaa053b6aa9d4a
WHawke <gothreepwood@gmail.com>
2025-07-23 12:28:21 +0100

    updated lang JSON to include the spanish

--- Assets/LegendsOfLearningSDK/WebGLLoLDemo/Scripts/Loader.cs
+++ Assets/LegendsOfLearningSDK/WebGLLoLDemo/Scripts/Loader.cs
@@ -52,9 +52,9 @@ namespace Pattern.Quest.Alpha.Phases.Games
             LOLSDK.Instance.GameStateChanged += new GameStateChangedHandler(HandleGameStateChange);
 
             // Mock the platform-to-game messages when in the Unity editor.
-#if UNITY_EDITOR
-          LoadMockData();
-#endif
+//#if UNITY_EDITOR
+//          LoadMockData();
+//#endif
 
             // Then, tell the platform the game is ready.
             LOLSDK.Instance.GameIsReady();
@@ -102,7 +102,7 @@ namespace Pattern.Quest.Alpha.Phases.Games
             // Either GameState.Paused or GameState.Resumed
             Debug.Log("HandleGameStateChange");
         }
- ///*      
+/*      
 #if UNITY_EDITOR
         private void LoadMockData()
         {
@@ -146,6 +146,6 @@ namespace Pattern.Quest.Alpha.Phases.Games
             }
         }
 #endif
- //*/
+*/
     }
 }
